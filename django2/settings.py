9efc801c142ca19277e2577d807d0cd30d5417d0
JairTex <steixeirajair@gmail.com>
2022-07-10 21:16:56 -0300

    Mudanças no Path

--- django2/settings.py
+++ django2/settings.py
@@ -129,12 +129,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media'
+STATIC_ROOT= str(BASE_DIR / 'staticfiles')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = str(BASE_DIR / 'media')
+
+'''
+STATIC_URL = '/static/'
+
+STATIC_ROOT= str(BASE_DIR.joinpath('staticfiles'))
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+'''
 #Configuarções de e-mail
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
